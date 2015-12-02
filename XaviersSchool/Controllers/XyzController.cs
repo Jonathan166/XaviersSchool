b32204f6b09dd9b9c21bc59b19a709d77dc4c092
Jonathan Langford <jlangf16@bruinmail.slcc.edu>
2015-12-02 11:38:24 -0700

    Added Xyz status

--- XaviersSchool/Controllers/XyzController.cs
+++ XaviersSchool/Controllers/XyzController.cs
@@ -14,31 +14,6 @@ namespace XaviersSchool.Controllers
         {
             var mutants = db.Mutants.ToList();
             return View(mutants);
-            //Ability physical = new Ability();
-            //physical.AbilityName = "Super Strength";
-
-            //Mutant charles = new Mutant();
-            //charles.FirstName = "Charles";
-            //charles.LastName = "Xavier";
-
-            //Mutant scott = new Mutant();
-            //scott.FirstName = "Scott";
-            //scott.LastName = "Summers";
-
-            //Mutant bobby = new Mutant();
-            //bobby.FirstName = "Bobby";
-            //bobby.LastName = "Drake";
-
-            //List<Mutant> mutants = new List<Mutant>();
-            //mutants.Add(charles);
-            //mutants.Add(scott);
-            //mutants.Add(bobby);
-
-            //Ability_Mutants obj = new Ability_Mutants();
-            //obj.ability = physical;
-            //obj.mutants = mutants;
-
-            //return View(obj);
         }
 
         public ActionResult Index()
